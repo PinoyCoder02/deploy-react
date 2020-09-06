a781f8894eac0d3abe1d67aa6543ba0f2ec0ceb9
PinoyCoder02 <sir.nikko.bernardino@gmail.com>
2020-09-06 12:10:53 +0800

    Updates

---
+++ precache-manifest.fe27562c5c6428dc63b84fb22f86f001.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "911af7ee404366a920f57af0a6b35704",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "c533badd597b58935f61",
+    "url": "/deploy-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/deploy-react/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c533badd597b58935f61",
+    "url": "/deploy-react/static/js/main.84b7c278.chunk.js"
+  },
+  {
+    "revision": "efc3c6837fa72e3006db",
+    "url": "/deploy-react/static/js/runtime-main.5dbc2b9c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
